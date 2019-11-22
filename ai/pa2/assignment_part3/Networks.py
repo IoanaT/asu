36e721c300c02c751567f720657c1ba3925f4f2b
Ioana <ioana.tiriac@gmail.com>
2019-11-22 15:44:19 +0100

    assignment part 3 with normalized data and 50 neurons in hidden layer

--- ai/pa2/assignment_part3/Networks.py
+++ ai/pa2/assignment_part3/Networks.py
@@ -2,14 +2,20 @@ import torch
 import torch.nn as nn
 
 class Action_Conditioned_FF(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size=6, hidden_size=50, outptut_size=1):
+        super(Action_Conditioned_FF, self).__init__()
+        self.input_to_hidden = nn.Linear(input_size, hidden_size)
+        self.nonlinear_activation = nn.Sigmoid()
+        self.hidden_to_output = nn.Linear(hidden_size, outptut_size)
 # STUDENTS: __init__() must initiatize nn.Module and define your network's
 # custom architecture
-        pass
 
     def forward(self, input):
 # STUDENTS: forward() must complete a single forward pass through your network
 # and return the output which should be a tensor
+        hidden = self.input_to_hidden(input)
+        hidden = self.nonlinear_activation(hidden)
+        output = self.hidden_to_output(hidden)
         return output
 
 
@@ -18,7 +24,11 @@ class Action_Conditioned_FF(nn.Module):
 # mind that we do not need to keep track of any gradients while evaluating the
 # model. loss_function will be a PyTorch loss function which takes as argument the model's
 # output and the desired output.
-        return loss
+        loss = 0
+        for idx, sample in enumerate(test_loader):
+            loss += loss_function(self.forward(sample['input']), sample['label'])
+        result = loss/len(test_loader)
+        return result
 
 def main():
     model = Action_Conditioned_FF()
